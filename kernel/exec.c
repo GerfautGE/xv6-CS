3404da2a63e98f00536538e50402220711f8ac21
Luc Videau <luc.videau@student-cs.fr>
2023-05-27 19:36:40 +0200

    remove uvmalloc use

--- kernel/exec.c
+++ kernel/exec.c
@@ -71,10 +71,6 @@ exec(char *path, char **argv)
       printf("exec: program header vaddr + memsz < vaddr\n");
       goto bad;
     }
-    if((sz = uvmalloc(pagetable, sz, ph.vaddr + ph.memsz)) == 0){
-      printf("exec: uvmalloc failed\n");
-      goto bad;
-    }
     if(ph.vaddr % PGSIZE != 0){
       printf("exec: vaddr not page aligned\n");
       goto bad;
